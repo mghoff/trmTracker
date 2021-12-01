7ee41b6d69873b55df52cc79e802a5a67707f4f2
Hoff <hoffm@energyharbor.com>
2021-12-01 08:34:20 -0500

    update username & password labels in Sys.getenv()

--- R/get_auth_token.R
+++ R/get_auth_token.R
@@ -1,8 +1,8 @@
 
 ### AUTHENTICATE WITH TRMTRACKER
 # Connect to TRM Tracker and get an Auth Token
-get_auth_token <- function(username = Sys.getenv("pioneer_usr"),
-                           password = Sys.getenv("pioneer_pwd"),
+get_auth_token <- function(username = Sys.getenv("TRM_usr"),
+                           password = Sys.getenv("TRM_pwd"),
                            verbose = FALSE) {
   results <- POST("https://app.trmtracker.com/TRMTracker_PROD/api/requesttoken",
                   add_headers("Content-Type" = "application/json"),
